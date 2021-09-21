e0aab2cd2814b3f9f48cf287231dea6f4022b191
theNarrt <89015313+theNarrt@users.noreply.github.com>
2021-09-21 08:32:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Bảo Trân à!',
+    introTitle: 'DƯơng à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: ' Luv u <3333333.',
+    title: 'MÌnh biết lỗi rồi mà 😙',
+    desc: 'Tha lỗi cho mình đi =(( ',
+    btnYes: 'Tha lỗi <33',
+    btnNo: 'Không,méo tha :3',
+    question:'Cho mình xin lũi nha <3',
+    btnReply: 'Gửi cho mình <3',
+    reply: 'Ok tha lỗi cho đó ',
+    mess: 'Cảm ơn bạn nhiều lắm 😘😘',
+    messDesc: ' Cảm ơn nha <3333333.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
